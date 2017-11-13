b7d1a58f1606f6c73d7f4176395da89348c76f62
Christine Murad <christine.murad@mail.utoronto.ca>
2017-11-12 22:08:29 -0500

    Fixed some spacing issues

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -5,30 +5,31 @@ const recognition = new SpeechRecognition();
 const socket = io();
 
 document.querySelector('button').addEventListener('click', () => {
-	recognition.start();
+    recognition.start();
 });
 
 recognition.addEventListener('result', (e) => {
-  	let last = e.results.length - 1;
-  	let text = e.results[last][0].transcript;
+    console.log(e.results);
+    let last = e.results.length - 1;
+    let text = e.results[last][0].transcript;
 
-  	console.log('Confidence: ' + e.results[0][0].confidence);
+    console.log('Confidence: ' + e.results[0][0].confidence);
 
-  	// We will use the Socket.IO here later…
-  	socket.emit('chat message', text);
+    // We will use the Socket.IO here later…
+    socket.emit('chat message', text);
 });
 
 function synthVoice(text) {
-  	const synth = window.speechSynthesis;
-  	const utterance = new SpeechSynthesisUtterance();
-  	var voices = synth.getVoices();
-  	console.log(voices);
-  	// utterance.voice = voices[];
-  	utterance.text = text;
-  	synth.speak(utterance);
+    const synth = window.speechSynthesis;
+    const utterance = new SpeechSynthesisUtterance();
+    var voices = synth.getVoices();
+    console.log(voices);
+    // utterance.voice = voices[];
+    utterance.text = text;
+    synth.speak(utterance);
 }
 
 socket.on('bot reply', function(replyText) {
-  	synthVoice(replyText);
+    synthVoice(replyText);
 });
 
